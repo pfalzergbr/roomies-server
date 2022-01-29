fc67141245b030e7c23310265c6371d87debaa6a
Gabor Pfalzer <pfalzer@gmail.com>
2022-01-29 13:18:10 +0000

    add pino-http typing

--- dist/app.js
+++ dist/app.js
@@ -7,8 +7,13 @@ const express_1 = __importDefault(require("express"));
 const config_1 = __importDefault(require("config"));
 const logger_1 = __importDefault(require("./utils/logger"));
 const pino_http_1 = __importDefault(require("pino-http"));
+const user_routes_1 = require("./user/user.routes");
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.use((0, pino_http_1.default)());
+app.use("/user", user_routes_1.userRouter);
 const port = config_1.default.get("port");
-app.listen(port, () => logger_1.default.info(`App is running on port ${port}`));
+app.listen(port, () => {
+    logger_1.default.info(`App is running on port ${port}`);
+});
 //# sourceMappingURL=app.js.map
---
+++ dist/user/user.controller.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRouteCheck = void 0;
+const userRouteCheck = (req, res) => {
+    res.status(200).send();
+};
+exports.userRouteCheck = userRouteCheck;
+//# sourceMappingURL=user.controller.js.map
