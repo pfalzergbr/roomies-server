ba36a9929cd50f260f3c1f62044db5a912cedd52
Gabor Pfalzer <pfalzer@gmail.com>
2022-02-01 08:15:55 +0000

    delete and ignore dist

--- dist/repositories/home.repository.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=home.repository.js.map
--- dist/user/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRouter = void 0;
-const express_1 = require("express");
-const user_controller_1 = require("./user.controller");
-const userController = new user_controller_1.UserController();
-exports.userRouter = (0, express_1.Router)();
-exports.userRouter.get("/check", userController.userRouterCheck);
-//# sourceMappingURL=user.routes.js.map
